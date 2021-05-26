936735e29681e56a11ada19bc1798b170fb029f5
Remi THOMAS <remi@xtware.com>
2021-05-26 07:45:32 +0200

    .

--- BlazorWheelZoomServer/Startup.cs
+++ BlazorWheelZoomServer/Startup.cs
@@ -45,7 +45,7 @@ namespace BlazorWheelZoomServer
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
